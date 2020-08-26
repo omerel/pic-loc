e96dd40df89652b6775cfeb7600343289e2c5513
Omer <elomer@gmail.com>
2020-08-26 14:19:42 +0300

    add user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_Sc0GFBill', 
+        userPoolClientId: 'enb3pmpjnbm4u23vmg2tkcopi',
+        region: 'eu-west-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
